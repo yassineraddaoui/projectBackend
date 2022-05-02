365c498383b7a09b1489e6222bc92fd353069c71
yassineraddaoui <yassineraddaouicr7@gmail.com>
2022-05-02 10:17:12 +0200

    ss2

---
+++ src/main/java/com/example/demo/model/Admin.java
@@ -0,0 +1,17 @@
+package com.example.demo.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Admin {
+	@Id
+	private String matricule;
+	private String password;
+}
